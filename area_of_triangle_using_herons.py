d5831dda6feb9fd67dee5380d9997d373692e942
Deepak <113306291+deepak7777787@users.noreply.github.com>
2023-11-04 23:40:49 +0530

    area of triangle, random number generation added

---
+++ area_of_triangle_using_herons.py
@@ -0,0 +1,11 @@
+# python program to calculate area of a triangle using Heron's formula
+
+a = float(input('Enter first side:'))
+b = float(input('Enter second side:'))
+c = float(input('Enter third side:'))
+
+s = (a + b + c) / 2
+
+area = (s*(s-a)*(s-b)*(s-c)) ** 0.5
+print('The area of the triangle = %0.2f' %area)
+
---
+++ generate_random_number.py
@@ -0,0 +1,7 @@
+# generating random number 
+
+# syntax ==> random.randint(a,b)
+
+import random
+
+print(random.randint(0,9))
